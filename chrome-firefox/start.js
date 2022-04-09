1ac1681a94ade43b063f2c1a45eb130ac0fe78ed
netanel <natanelhenya@gmail.com>
2022-04-09 23:01:04 +0300

    added Nyaa as service and some bug fix

--- chrome-firefox/start.js
+++ chrome-firefox/start.js
@@ -1,35 +1,38 @@
-const getServices = (title) => {
-  return [
-    {
-      name: "Yify",
-      url: `https://yts.mx/browse-movies/${title}`,
-      icon: "https://yts.mx/assets/images/website/favicon.ico",
-    },
-    {
-      name: "Rarbg",
-      url: `https://rarbgtor.org/torrents.php?category=movies&search=${title}`,
-      icon: "https://rarbg.to/favicon.ico",
-    },
-    {
-      name: "1337x",
-      url: `https://www.1377x.to/search/${title}/1`,
-      icon: "https://1337xto.to/images/favicon.ico",
-    },
-    {
-      name: "Lime Torrents",
-      url: `https://limetorrents.cyou/search.php?cat=201&q=${title}`,
-      icon: "https://limetorrents.cyou/favicon.ico",
-    },
-    {
-      name: "YouTube",
-      url: `https://www.youtube.com/results?search_query=${title}`,
-      icon: "https://www.youtube.com/favicon.ico",
-    },
-  ];
-};
+const getServices = (title) => [
+  {
+    name: "Yify",
+    url: `https://yts.mx/browse-movies/${title}`,
+    icon: "https://yts.mx/assets/images/website/favicon.ico",
+  },
+  {
+    name: "Rarbg",
+    url: `https://rarbgtor.org/torrents.php?category=movies&search=${title}`,
+    icon: "https://rarbg.to/favicon.ico",
+  },
+  {
+    name: "1337x",
+    url: `https://www.1377x.to/search/${title}/1`,
+    icon: "https://1337xto.to/images/favicon.ico",
+  },
+  {
+    name: "Lime Torrents",
+    url: `https://limetorrents.cyou/search.php?cat=201&q=${title}`,
+    icon: "https://limetorrents.cyou/favicon.ico",
+  },
+  {
+    name: "Nyaa",
+    url: `https://nyaa.si/?f=0&c=0_0&q=${title}`,
+    icon: "https://nyaa.si/static/favicon.png",
+  },
+  {
+    name: "YouTube",
+    url: `https://www.youtube.com/results?search_query=${title}`,
+    icon: "https://www.youtube.com/favicon.ico",
+  },
+];
 
 const addService = (service) => {
-  let services = document.getElementsByClassName("services")[0];
+  const services = document.getElementsByClassName("services")[0];
 
   services.innerHTML += `
 		<p class="service">
@@ -46,40 +49,42 @@ const addService = (service) => {
 };
 
 const hideOther = () => {
-  let servicesPanel = document.getElementsByClassName("services")[0];
+  const servicesPanel = document.getElementsByClassName("services")[0];
   servicesPanel.innerHTML = ""; // removes services content
   document.querySelectorAll(".other").forEach((a) => {
     a.remove(); // removes every element with the class 'other'
   });
 };
 
-const getTitle = () => {
-  let title = document.getElementsByClassName(
+const getQuery = () => {
+  const title = document.getElementsByClassName(
     "headline-1 js-widont prettify"
   )[0].innerText;
 
-  let year = document
+  const year = document
     .getElementsByClassName("number")[0]
     .getElementsByTagName("a")[0].innerText;
 
   return `${title} ${year}`;
 };
 
-const init = (num) => {
-  var styles = `
-.watch-panel .services>.service:nth-child(n+${num}) {
-  display: block !important;
-}
+const init = () => {
+  const styleString = `
+  .watch-panel .services .service {
+    display: flex !important; 
+  }
 `;
-  var styleSheet = document.createElement("style");
-  styleSheet.innerText = styles;
-  document.head.appendChild(styleSheet);
+
+  const style = document.createElement("style");
+  style.textContent = styleString;
+  document.head.append(style);
 };
 
 const insertServices = () => {
   hideOther();
-  let services = getServices(getTitle());
-  init(services.length);
+  const query = getQuery();
+  const services = getServices(query);
+  init();
 
   for (const service of services) {
     addService(service);
